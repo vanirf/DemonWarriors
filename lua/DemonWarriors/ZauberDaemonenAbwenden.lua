2121d9bd32a7a7f1336d3d51a8d6d36f66b6e27d
vanirf <milorad.mijailovic@web.de>
2015-10-01 10:49:41 +0200

    DaemonenAbwenden getriggert, dafuer noetiges Scriptfile mit 2 Funktionen zur Gruppentrennung, de, gegena aus, und ausfuehrung des zaubers, sowie um den wachraum zu verlassen erstellt.

---
+++ lua/DemonWarriors/ZauberDaemonenAbwenden.lua
@@ -0,0 +1,11 @@
+require("DemonWarriors.DemonNames")
+function ZauberDaemonenAbwenden()
+world.Execute("de")
+world.Execute("gegena aus")
+world.Execute("g verlasse")
+world.Execute("t bullrik")
+world.Execute("z daemonenabwenden bullrik")
+end
+function DaemonenAbwendenEnde()
+world.Execute("s")
+end
